e7abe89dc389e1b36392054ce8bf7206eddfbec4
SeanWelshBrown <seanwelshbrown@gmail.com>
2020-03-02 10:01:06 -0500

    Updated DB to PostgreSQL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,7 +51,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "everybody_loves_fractals_production"
+  # config.active_job.queue_name_prefix = "everybody_loves_fractals_backend_production"
 
   config.action_mailer.perform_caching = false
 
